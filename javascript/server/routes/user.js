094d3e28fb83b17badd0ada4083821bcf6c5e1a7
hj2222222 <111670814+hj2222222@users.noreply.github.com>
2024-05-02 10:06:01 +0900

    전화번호 인증
    인증 코드 및 링크 추가

--- javascript/server/routes/user.js
+++ javascript/server/routes/user.js
@@ -11,6 +11,34 @@ router.get('/',(req,res)=>{
     res.status(200).send('회원정보 확인')
 })
 
+router.post('/number',(req,res)=>{
+    let authNumbers = '';
+
+    async function authNumber(){
+        for(let i=0;i<4;i++){
+            let num = Math.floor(Math.random()*10).toString()
+            authNumbers += num
+    }
+    console.log(authNumbers)
+    }
+
+
+    async function printTokenResult(phone, token){
+
+        const mysms = coolsms.default;
+        const messageService = new mysms("NCSNZ3LCEPD0B6F9","CPMZZ3BLKI7LPOCIV8X4WYXWWGCTVTFA");
+        const result = await messageService.sendOne({
+            to: `${phone}`,
+            from : '01084454689',
+            text : `요청하신 인증번호는 [${token}]입니다.`
+        })
+
+    console.log(result);
+}
+
+    res.status(201).send('휴대폰 문자 인증')
+})
+
 router.post('/',(req,res)=>{
     res.status(201).send('회원가입')
 })
